b2b7188dce303eba2de5b983c4f78c13fbc14176
Christian Camargo Del Moro <christian.delmor@gmail.com>
2023-03-09 20:41:25 -0300

    Basic styling of contact page.

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	name = forms.CharField(max_length=255, required=True)
-	from_email = forms.EmailField(required=True)
-	subject = forms.CharField(required=True)
-	message = forms.CharField(widget=forms.Textarea, required=True)
+	name = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class':'text-input-half-width', 'placeholder':'Name *'}))
+	from_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'text-input-half-width', 'placeholder':'Your E-mail *'}))
+	subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'text-input-full-width', 'placeholder':'Subject *'}))
+	message = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'text-input-full-width', 'placeholder':'Message *'}))
