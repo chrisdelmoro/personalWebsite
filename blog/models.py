0a6269a33d39f4e87254ad3851818c94c18f850b
Christian Camargo Del Moro <christian.delmor@gmail.com>
2023-03-26 15:38:49 -0300

    Post images are now organized inside the media folder. They are organized as follow: category/post title/

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,9 @@ class Category(models.Model):
 		return '/%s/' % self.slug
 
 
+def post_header_path(instance, filename):
+	return '{0}/{1}'.format(instance.category.title + '/' + instance.title, filename)
+
 class Post(models.Model):
 	ACTIVE = 'active'
 	DRAFT = 'draft'
@@ -30,7 +33,7 @@ class Post(models.Model):
 	category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
 	title = models.CharField(max_length=255)
 	slug = models.SlugField()
-	header_img = models.ImageField(blank=True, null=True, upload_to='images/')
+	header_img = models.ImageField(blank=True, null=True, upload_to=post_header_path)
 	intro = RichTextField()
 	body = RichTextField()
 	created_at = models.DateTimeField(auto_now_add = True)
@@ -46,9 +49,12 @@ class Post(models.Model):
 		return '/%s/%s/' % (self.category.slug, self.slug)
 
 
+def post_images_path(instance, filename):
+	return '{0}/{1}'.format(instance.post.category.title + '/' + instance.post.title, filename)
+
 class PostImages(models.Model):
 	post = models.ForeignKey(Post, related_name='images', on_delete=models.CASCADE)
-	image = models.ImageField(blank=True, null=True, upload_to='images/')
+	image = models.ImageField(blank=True, null=True, upload_to=post_images_path)
 
 	class Meta:
 		verbose_name_plural: 'PostImages'
